1c43f1149e577f1ac8406f23dc3b9c2aea7da543
William <williamh29@nycstudents.net>
2022-10-28 23:51:06 -0400

    initialize

--- src/Cat.java
+++ src/Cat.java
@@ -1,43 +1,32 @@
-public class Cat
-{
+public class Cat {
     private String name;
     private int age;
 
-    public Cat(String name, int age)
-    {
+    public Cat(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    public String getName()
-    {
+    public String getName() {
         return name;
     }
 
-    public String getAge() {
+    public int getAge() {
         return age;
     }
 
-    // add a getAge() getter below for
-    // obtaining the Cat's age
-
-
-
-    // MODIFY this method so that the the Cat randomly
-    // returns one of three different messages
-    // (make any messages you want!) rather than always returning "meow".
-    // Each message should have equal chance of being returned.
-    public String speak()
-    {
-        int random = (int) (Math.random() * 3 ) + 1;
-        if (random ==1)  {
+    public String speak() {
+        int random = (int) (Math.random() * 3) + 1;
+        if (random == 1) {
             return "purrrr";
+        } else if (random == 2) {
+            return "meow!";
+        } else {
+            return "mouse!";
         }
-        return "meow!";
     }
 
-    public void introduce()
-    {
-        System.out.println("Hello. My name is " + name + " and I am " + age + " years old. Now, mind your own business.");
+        public void introduce() {
+            System.out.println("Hello. My name is " + name + " and I am " + age + " years old. Now, mind your own business.");
+        }
     }
-}
